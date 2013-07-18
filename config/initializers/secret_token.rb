e5e5d98e9e63945722ec5792a502270cf4f600f7
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2013-07-18 17:12:05 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestappCasper::Application.config.secret_key_base = '8b3cdfe3ce137953bf563ba276aca6c84c822b5bb715e90642f871acd15d2c4e1cb8ce2f4750d3e17de9710bffb7faa6182c1550e0faeda330d1e35cecbb5c6c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestappCasper::Application.config.session_store :cookie_store, key: '_testapp-casper_session'
